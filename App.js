0f5c9db1d286c3fe45bc4c7819dcde8857f25bdc
Jeremy Chee <jeremychee97@gmail.com>
2024-01-07 15:59:30 +0800

    Main Page and header buttons
    Changed first screen to Main Page and modified header buttons to direct back to main page

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { DetailsScreen } from './components/DetailsScreen';
 import { AppTheme } from './styles/Theme';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { MainScreen } from "./components/MainScreen";
 
 const Stack = createNativeStackNavigator()
 
@@ -15,25 +16,29 @@ function App() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <NavigationContainer theme={AppTheme}>
-          <Stack.Navigator initialRouteName="Home" backgroundColor="">
-            <Stack.Screen name="Home" component={QuestionGroup} options={{
+          <Stack.Navigator initialRouteName="Main" backgroundColor="">
+            <Stack.Screen name="Main" component={MainScreen} />
+            <Stack.Screen name="Home" component={QuestionGroup} options={({ navigation }) => ({
               title: 'New Poll',
               headerRight: () => (
                 // Should be disabled if: 
                 // 1. A question is not provided
                 // 2. There are less than 2 options available
                 // 3. Inputs do not exceed their text limit
-                <Button onPress={() => alert('TODO: Give an alert saying poll submitted Redirect back to home screen')}
+                <Button onPress={() => {
+                  alert('TODO: Give an alert saying poll submitted Redirect back to home screen')
+                  navigation.navigate('Main')
+                }}
                   title="Send"
                   color='#617A67'
                 />
               ),
               headerLeft: () => (
-                <Button onPress={() => alert('TODO: Redirect back to home screen')}
+                <Button onPress={() => navigation.navigate('Main')}
                   title="Cancel"
                   color="#617A67" />
               )
-            }} />
+            })} />
             <Stack.Screen name="Details" component={DetailsScreen} />
           </Stack.Navigator>
         </NavigationContainer>
---
+++ components/MainScreen.js
@@ -0,0 +1,10 @@
+import { Button, Text, View } from "react-native";
+
+export function MainScreen({ navigation }) {
+    return (
+        <View>
+            <Text>Welcome to the Poll Bank</Text>
+            <Button title=" Create a new poll" onPress={() => navigation.navigate('Home')} />
+        </View>
+    )
+}
