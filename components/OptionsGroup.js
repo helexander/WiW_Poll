51bd5fc3695de22b57e0cf9f7a798f0bac603023
Jeremy Chee <jeremychee97@gmail.com>
2024-01-07 15:32:04 +0800

    Working version after adding draggable list

--- components/OptionsGroup.js
+++ components/OptionsGroup.js
@@ -1,24 +1,73 @@
-import { StyleSheet, Text, View } from "react-native"
-import DraggableFlatList from "react-native-draggable-flatlist"
+import React, { useState } from "react";
+import { Text, StyleSheet, TouchableOpacity, View } from "react-native";
+import DraggableFlatList, {
+    ScaleDecorator,
+} from "react-native-draggable-flatlist";
 
-export function OptionsGroup({ navigation }) {
+const getColor = (i, numItems) => {
+    const multiplier = 255 / (numItems - 1);
+    const colorVal = i * multiplier;
+    return `rgb(${colorVal}, ${Math.abs(128 - colorVal)}, ${255 - colorVal})`;
+}
+
+const mapIndexToData = (d, index, arr) => {
+    const backgroundColor = getColor(index, arr.length);
+    return {
+        text: `${index}`,
+        key: `key-${backgroundColor}`,
+        backgroundColor,
+    };
+}
+
+const NUM_ITEMS = 10;
+
+const initialData = [...Array(NUM_ITEMS)].map(mapIndexToData);
+
+export function OptionsGroup() {
+    const [data, setData] = useState(initialData);
+
+    const renderItem = ({ item, drag, isActive }) => {
+        return (
+            <ScaleDecorator>
+                <TouchableOpacity
+                    activeOpacity={1}
+                    onLongPress={drag}
+                    disabled={isActive}
+                    style={[
+                        styles.rowItem,
+                        { backgroundColor: isActive ? "red" : item.backgroundColor },
+                    ]}
+                >
+                    <Text style={styles.text}>{item.text}</Text>
+                </TouchableOpacity>
+            </ScaleDecorator>
+        );
+    };
 
     return (
         <View>
-            <Text>Options</Text>
-            {/* <DraggableFlatList /> */}
+            <Text style={{ marginBottom: 8 }}>Options</Text>
+            <DraggableFlatList
+                data={data}
+                onDragEnd={({ data }) => setData(data)}
+                keyExtractor={(item) => item.key}
+                renderItem={renderItem}
+                height={400}
+            />
         </View>
-    )
+    );
 }
 
-const inputBoxStyle = StyleSheet.create({
-    input: {
-        fontSize: 16,
-        margin: 8,
-        borderRadius: 8,
-        padding: 12,
-        paddingTop: 12,
-        paddingBottom: 16,
-        backgroundColor: 'white',
-    }
-})
+const styles = StyleSheet.create({
+    rowItem: {
+        height: 100,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        color: "white",
+        fontSize: 24,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+});
